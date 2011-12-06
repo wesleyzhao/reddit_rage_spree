0d5ebc811d5500fa3923ecae0b723d2579f0b59e
Wesley Zhao <wesley.zhao@gmail.com>
2011-12-06 08:47:05 -0800

    changed it to a pageAction and fixed a bug

--- bg_script.js
+++ bg_script.js
@@ -1,20 +1,36 @@
 var links = Array();
 
 chrome.extension.onConnect.addListener(function(port){
+    console.log ('here inside the port connect');
     var tab = port.sender.tab;
-    
+    var popup_file = "f7u12.html";
     port.onMessage.addListener(function(info){
+	console.log('here inside the addlistener port.onMessage');
 	links = info.anchors;
 	if (links){
-	    var popup_file = "popup.html";
+	    console.log('here inside the if links True');
+	    popup_file = "popup.html";
 	}
 	else{
-	    var popup_file = "f7u12.html";
+	    console.log('here inside the if links False');
+	    popup_file = "f7u12.html";
 	}
 	
 	chrome.tabs.getSelected(null, function(tab){
-	    chrome.browserAction.setPopup({tabId: tab.id, "popup" : popup_file});
+	    console.log('here inside the tabs.getSelected');
+	    chrome.pageAction.setPopup({tabId: tab.id, "popup" : popup_file});
         });//end getSelected
     });//end addListener
 });//end
      
+
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
+// will check if page is reddit, if so, then will show the pageAction popup icon
+
+function checkForValidUrl(tabId, changeInfo, tab){
+
+    if (tab.url.indexOf('http://www.reddit.com') > -1){
+	chrome.pageAction.show(tabId);
+    }
+
+}
